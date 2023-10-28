583e8d023f46821e34f21bce48bfd9642f1fd003
Zoran Jankovic <zoran.jankovic@uns.ac.rs>
2023-10-28 12:11:03 +0200

    remove comments

--- ECommerce/Solution/Application/Common/Queries/IGetListQuery.cs
+++ ECommerce/Solution/Application/Common/Queries/IGetListQuery.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 
 namespace Application.Common.Queries
 {
-    // SOLID Explanation: Dependency Inversion Principle
-    // Abstractions should not depend upon  details. Details should depend upon  abstractions.
-    // Imamo listu entiteta, tako da ova apstrakcija ne zavisi ni od jedne implementacije na primer SQL- i ni od jednog konkretnoog entiteta, dakle ni od jednog detalja.
-    // Mozemo podjednako da koristimo svaku pojedinacnu implementaciju ovog interfejsa, za bilo koji entitet, i za bilo koju perzistenciju (in-memory, MS SQL Server, NoSQL...)
     public interface IGetListQuery<TEntity> where TEntity : Entity
     {
         List<TEntity> GetList();
